1d00e762370f5dc5c418cb1448f59df8d7ddefe5
Lucas Berbesson <lucasberbesson@gmail.com>
2024-04-29 15:02:52 +0200

    fix height

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -57,7 +57,7 @@ export const HeroParallax = ({
     return (
         <div
             ref={ref}
-            className="h-[320vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+            className=" overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d] [height:30fix 00px]"
         >
             <Header/>
             <motion.div
