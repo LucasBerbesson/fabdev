26438f64b25418d0ad601e8aacc3e145463de174
Lucas Berbesson <lucasberbesson@gmail.com>
2024-12-27 16:35:16 +0100

    No cases cache

--- app/cases/api/route.tsx
+++ app/cases/api/route.tsx
@@ -2,6 +2,8 @@ export async function GET() {
     const res = await fetch(`https://backoffice.fabdev.fr/api/cases/`, {
         headers: {
             'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',       // Additional no-cache directive
         },
     })
     const data = await res.json()
